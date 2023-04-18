af60ab3d6ff493f1ba97a0214cc214998cbe4657
Nikita <w.nnovikov@gmail.com>
2023-04-18 22:34:40 +0300

    add MainWin and modified styles

--- app/src/main.py
+++ app/src/main.py
@@ -1,7 +1,9 @@
 from PyQt6 import QtCore
+import os
 
-def main():
-    print('Основная функция приложения')
+
+def main():  # Основная функция приложения
+    os.system('python app/src/MainWin.py')
 
 
 if __name__ == '__main__':
