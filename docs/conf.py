a4491f78c88679258ac7d87eb8d7304b9bc199ce
nnovi <wpnnow100@gmail.com>
2024-04-11 10:27:25 +0300

    create docs Sphinx

---
+++ docs/conf.py
@@ -0,0 +1,35 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+import sys
+import os
+
+sys.path.insert(0, os.path.abspath('.'))
+sys.path.insert(0, os.path.abspath('..'))
+sys.path.insert(0, os.path.abspath('../app/src/logic'))
+sys.path.insert(0, os.path.abspath('../app/src/logic/controller'))
+
+project = 'Kizana MemoryCards'
+copyright = '2024, Kizana MemoryCards'
+author = 'nnovi'
+release = '2.1'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.napoleon']
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+language = 'ru'
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'sphinx_book_theme'
+html_static_path = ['_static']
---
+++ docs/modules.rst
@@ -0,0 +1,57 @@
+Модули
+=======
+
+* :ref:`modindex`
+* :ref:`search`
+
+.. automodule:: Controller
+    :members:
+    :undoc-members:
+
+.. automodule:: DataBaseManager
+    :members:
+    :undoc-members:
+
+.. automodule:: application
+    :members:
+    :undoc-members:
+
+.. automodule:: main_window
+    :members:
+    :undoc-members:
+
+.. automodule:: side_menu
+    :members:
+    :undoc-members:
+
+.. automodule:: dicts_side_menu
+    :members:
+    :undoc-members:
+
+.. automodule:: parameters_side_menu
+    :members:
+    :undoc-members:
+
+.. automodule:: settings_side_menu
+    :members:
+    :undoc-members:
+
+.. automodule:: edit_dict_panel
+    :members:
+    :undoc-members:
+
+.. automodule:: play_panel
+    :members:
+    :undoc-members:
+
+.. automodule:: answer_mode
+    :members:
+    :undoc-members:
+
+.. automodule:: view_mode
+    :members:
+    :undoc-members:
+
+.. automodule:: result_window
+    :members:
+    :undoc-members:
